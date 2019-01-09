85171494def7bda1ce8c0d51bbcea39b3cac7da4
kebablyulya <yaayulya@yandex.ru>
2019-01-09 22:28:44 +0300

    Added view and template for detailed blog post as well as CSS...

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.service_list, name='service_list'),
+    url(r'^service/(?P<pk>\d+)/$', views.service_detail, name='service_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
-from django.utils import timezone
+#from django.utils import timezone
 from django.contrib.auth.models import User
+
 
 
 def service_list(request):
     me = User.objects.get(username='kebablyulya')
     services = Service.objects.filter(author=me)
     return render(request, 'blog/service_list.html', {'services': services})
+
+
+def service_detail(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+    return render(request, 'blog/service_detail.html', {'service': service})
 
 # Create your views here.
 
