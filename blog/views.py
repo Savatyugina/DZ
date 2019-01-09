b9f709c64d531f3decf5b28182e5ea78b30bf47c
kebablyulya <yaayulya@yandex.ru>
2019-01-09 21:45:05 +0300

    jhgjd

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Service
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 def service_list(request):
-    services = Service.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    me = User.objects.get(username='kebablyulya')
+    services = Service.objects.filter(author=me)
     return render(request, 'blog/service_list.html', {'services': services})
 
 # Create your views here.
+
