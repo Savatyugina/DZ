545a66ba9bd255185c8d4e17f0f7e035999d160a
kebablyulya <yaayulya@yandex.ru>
2019-01-09 23:17:44 +0300

    Added views to create/edit blog ...

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.service_list, name='service_list'),
     url(r'^service/(?P<pk>\d+)/$', views.service_detail, name='service_detail'),
+    url(r'^service/new/$', views.service_new, name='service_new'),
+    url(r'^service/(?P<pk>\d+)/edit/$', views.service_edit, name='service_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Service
-#from django.utils import timezone
+from django.utils import timezone
 from django.contrib.auth.models import User
-
+from .forms import ServiceForm
 
 
 def service_list(request):
@@ -14,6 +14,35 @@ def service_list(request):
 def service_detail(request, pk):
     service = get_object_or_404(Service, pk=pk)
     return render(request, 'blog/service_detail.html', {'service': service})
+
+
+def service_new(request):
+    if request.method == "SERVICE":
+        form = ServiceForm(request.SERVICE)
+        if form.is_valid():
+            service = form.save(commit=False)
+            service.author = request.user
+            service.published_date = timezone.now()
+            service.save()
+            return redirect('service_detail', pk=service.pk)
+        else:
+            form = ServiceForm()
+        return render(request, 'blog/service_edit.html', {'form': form})
+
+
+def service_edit(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+    if request.method == "POST":
+        form = ServiceForm(request.POST, instance=service)
+        if form.is_valid():
+            service = form.save(commit=False)
+            service.author = request.user
+            service.published_date = timezone.now()
+            service.save()
+            return redirect('service_detail', pk=service.pk)
+    else:
+        form = ServiceForm(instance=service)
+    return render(request, 'blog/service_edit.html', {'form': form})
 
 # Create your views here.
 
